131fd0b8f0fa116521d89807a3f4c801ec421424
Dex-Enthusiast <ahmadijaka89@gmail.com>
2025-12-02 11:43:34 +0700

    FEAT : Update backend untuk otomatis mendeteksi proyek yang lewat tanggal waktu deadline

--- jadwal-proyek/api/read.php
+++ jadwal-proyek/api/read.php
@@ -6,8 +6,14 @@ $query = "SELECT * FROM projects ORDER BY id DESC";
 $result = mysqli_query($koneksi, $query);
 $response = [];
 
+$hari_ini = date('Y-m-d');
 
 while ($row = mysqli_fetch_assoc($result)) {
+    if ($row['status'] != 'Selesai' && $row['selesai'] < $hari_ini) {
+        // manipulasi outputnya (Gak ngerubah database, cuma tampilan)
+        $row['status'] = 'Telat';
+    }
+    
     $response[] = $row;
 }
 
